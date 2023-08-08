9d9ef60c1825b697c7734e53cc284a08954a9c39
gobinath90 <ravi.gopi90@gmail.com>
2023-08-08 11:00:33 +0530

    FrameworkConstants
    FrameworkConstants

---
+++ src/main/java/constants/FrameworkConstants.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class FrameworkConstants {
+	public static final int EXPLICIT_WAIT = 20;
+	public static String ENVIRONMENT = System.getProperty("env", "browserstack");
+	// gobinath
+	public static final String USERNAME = "gobinathr_ZhBote";
+	public static final String ACCESS_KEY = "46eZ3dHgS7jXSeXi1szr";
+	// Radhakrishnan
+//	public static final String USERNAME = "radhakrishnan_ST135E";
+//	public static final String ACCESS_KEY = "BTtSx4GFE4VTXpGHxzTS";
+	public static final String URL_BROWSERSTACK = "@hub-cloud.browserstack.com/wd/hub";
+	
+		public static final String MAIN_URL = "https://qa.beta.etana.com";
+}
