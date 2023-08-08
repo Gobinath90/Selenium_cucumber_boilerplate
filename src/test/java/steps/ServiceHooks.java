9e7dccc98dd76c89ddb6d5dc3deceabdc9e09660
gobinath90 <ravi.gopi90@gmail.com>
2023-08-08 11:02:07 +0530

    reports
    reports

---
+++ src/test/java/steps/ServiceHooks.java
@@ -0,0 +1,54 @@
+package steps;
+
+
+import static constants.FrameworkConstants.MAIN_URL;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import support.Driver;
+import utils.Filters;
+
+public class ServiceHooks {
+	   Filters filter = new Filters();
+	   WebDriver driver;
+
+
+    @Before
+    public void beforeScenario(Scenario scenario) throws InterruptedException, NoSuchFieldException, IllegalAccessException, MalformedURLException {
+    	
+    	System.out.println("BEFORE: THREAD ID : " + Thread.currentThread().getId() + "," + "SCENARIO NAME: " + scenario.getName());
+    	
+    	filter.removeDirectoryEvidence();
+//         if (ENVIRONMENT.equals("browserstack")) {
+//             Driver.createBrowserStack().get(MAIN_URL);
+//         }
+//         else {
+//             Driver.getDriver().get(MAIN_URL);
+//         }
+         Driver.getDriver().get(MAIN_URL);
+    }
+
+
+    @After
+    public void tearDown(Scenario scenario) throws InterruptedException {
+    	   	
+    	System.out.println(
+				"AFTER: THREAD ID : " + Thread.currentThread().getId() + "," + "SCENARIO NAME: " + scenario.getName());
+    	Driver.AddScreenshot(scenario);
+        Driver.endSession();
+    }
+
+    
+//    @AfterStep
+//    public void afterStep() {
+//        Screenshot.takeScreenShot();
+//    }
+   
+    
+}
+
